3bcd129fb9f3631948c6eb2d72543087dc7442a5
Daniil Belyakov <daniil@bunq.com>
2017-01-29 17:26:03 +0100

    add documentation for DisplayObject

--- js/app/engine/graphics/DisplayObject.js
+++ js/app/engine/graphics/DisplayObject.js
@@ -1,9 +1,16 @@
-define([
-    ],
-    function (
-        ) {
+define(
+    [],
+    function () {
         'use strict';
 
+        /**
+         * @param {number} x
+         * @param {number} y
+         * @param {number} width
+         * @param {number} height
+         *
+         * @constructor
+         */
         function DisplayObject(x, y, width, height) {
             this.x = x;
             this.y = y;
@@ -11,37 +18,62 @@ define([
             this.height = height;
         }
 
+        /**
+         * @param {number} x
+         */
         DisplayObject.prototype.setX = function (x) {
             this.x = x;
-        }
+        };
 
+        /**
+         * @returns {number}
+         */
         DisplayObject.prototype.getX = function () {
             return this.x;
-        }
+        };
 
+        /**
+         * @param {number} y
+         */
         DisplayObject.prototype.setY = function (y) {
             this.y = y;
-        }
+        };
 
+        /**
+         * @returns {number}
+         */
         DisplayObject.prototype.getY = function () {
             return this.y;
-        }
+        };
 
+        /**
+         * @param {number} width
+         */
         DisplayObject.prototype.setWidth = function (width) {
             this.width = width;
-        }
+        };
 
+        /**
+         * @returns {number}
+         */
         DisplayObject.prototype.getWidth = function () {
             return this.width;
-        }
+        };
 
+        /**
+         * @param {number} height
+         */
         DisplayObject.prototype.setHeight = function (height) {
             this.height = height;
-        }
+        };
 
+        /**
+         * @returns {number}
+         */
         DisplayObject.prototype.getHeight = function () {
             return this.height;
-        }
+        };
 
         return DisplayObject;
-    });
+    }
+);
