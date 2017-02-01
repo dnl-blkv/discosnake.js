925a78037d588fe7b4e8a87c0f76b348078557d9
Daniil Belyakov <daniil@bunq.com>
2017-02-01 12:30:06 +0100

    add JSDoc

--- js/app/game/ui/ScoreBoard.js
+++ js/app/game/ui/ScoreBoard.js
@@ -7,21 +7,36 @@ define([
         'use strict';
         var TextObject = engine.graphics.TextObject;
 
+        /**
+         * @constructor
+         */
         function ScoreBoard() {
             this.score = 0;
-            var scoreFontSize = 72;
-            var scoreX = 20;
-            var scoreY = scoreFontSize / 2;
-            var scoreText = '' + this.score;
-            var scoreColor = '#999999';
-            var scoreFontFamily = 'Wendy';
-            var scoreScreen = new TextObject(scoreText, scoreFontSize, scoreFontFamily, scoreColor);
-            scoreScreen.setX(scoreX);
-            scoreScreen.setY(scoreY);
-            this.scoreScreen = scoreScreen;
+            this.scoreScreen = createScoreScreen(this.score);
             this.updateHtmlStyle();
         }
 
+        /**
+         * @param scoreCount
+         *
+         * @returns {engine.graphics.TextObject}
+         */
+        function createScoreScreen(scoreCount) {
+            var scoreFontSize = 72;
+            var scoreX = 20;
+            var scoreY = scoreFontSize / 2;
+            var scoreColor = '#999999';
+            var scoreFontFamily = 'Wendy';
+            var scoreScreen = new TextObject('' + scoreCount, scoreFontSize, scoreFontFamily, scoreColor);
+            scoreScreen.setX(scoreX);
+            scoreScreen.setY(scoreY);
+
+            return scoreScreen;
+        }
+
+        /**
+         * @param {ScoreBoard} scoreBoard
+         */
         function updateScore(scoreBoard) {
             var scoreScreen = scoreBoard.scoreScreen;
             var scoreText = '' + scoreBoard.score;
@@ -29,41 +44,59 @@ define([
             scoreScreen.setText(scoreText);
         }
 
+        /**
+         */
         ScoreBoard.prototype.reset = function() {
             this.score = 0;
             updateScore(this);
         };
 
+        /**
+         * @returns {Number}
+         */
         ScoreBoard.prototype.getScore = function() {
             return this.score;
         };
 
+        /**
+         * @param {Number} difference
+         */
         ScoreBoard.prototype.changeScore = function(difference) {
             this.score += difference;
             updateScore(this);
         };
 
+        /**
+         */
         ScoreBoard.prototype.incrementScore = function() {
             this.changeScore(1);
 
         };
 
+        /**
+         */
         ScoreBoard.prototype.decrementScore = function() {
             this.changeScore(-1);
         };
 
+        /**
+         * @param {Graphics} gameGraphics
+         */
         ScoreBoard.prototype.draw = function(gameGraphics) {
             this.scoreScreen.draw(gameGraphics);
         };
 
+        /**
+         * @returns {HTMLElement}
+         */
         ScoreBoard.prototype.getHtml = function() {
             return this.scoreScreen.getHtml();
         };
 
+        /**
+         */
         ScoreBoard.prototype.updateHtmlStyle = function() {
-            var html = this.getHtml();
-
-            html.style.left = '24px';
+            this.getHtml().style.left = '24px';
         };
 
         return ScoreBoard;
