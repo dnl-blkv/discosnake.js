31de5d38c52c352e9c1b1a1f513818721423c7f2
Daniil Belyakov <daniil@bunq.com>
2017-02-16 11:11:22 +0100

    add more unit testing, fix test file name

--- src/engine/graphics/Color.js
+++ src/engine/graphics/Color.js
@@ -32,6 +32,79 @@ define([],
         }
 
         /**
+         * @param {string} hexString
+         *
+         * @returns {Color}
+         */
+        Color.createFromHexString = function(hexString) {
+            var colorNumberHex = hexString.substring(1);
+            var colorChannelsHex = colorNumberHex.match(/.{2}/g);
+            var red = parseInt(colorChannelsHex[0], 16);
+            var green = parseInt(colorChannelsHex[1], 16);
+            var blue = parseInt(colorChannelsHex[2], 16);
+
+            return (new Color(red, green, blue));
+        };
+
+        /**
+         * @returns {Color}
+         */
+        Color.prototype.copy = function() {
+            return new Color(this.getRed(), this.getGreen(), this.getBlue());
+        };
+
+        /**
+         * Builds a color object given HSV values in range [0..1].
+         *
+         * @param {number} hue
+         * @param {number} saturation
+         * @param {number} value
+         *
+         * @returns {Color}
+         */
+        Color.createFromHSV = function(hue, saturation, value) {
+            var hueInterval = Math.floor(hue * 6);
+            var f = hue * 6 - hueInterval;
+            var p = value * (1 - saturation);
+            var q = value * (1 - f * saturation);
+            var t = value * (1 - (1 - f) * saturation);
+            var coefficients = determineRgbCoefficients(hueInterval, value, p, q, t);
+            var red = Math.round(256 * coefficients[0]);
+            var green = Math.round(256 * coefficients[1]);
+            var blue = Math.round(256 * coefficients[2]);
+
+            return new Color(red, green, blue);
+        };
+
+        /**
+         * @param {number} hueInterval
+         * @param {number} value
+         * @param {number} p
+         * @param {number} q
+         * @param {number} t
+         *
+         * @returns {number[]}
+         */
+        function determineRgbCoefficients(hueInterval, value, p, q, t) {
+            switch (hueInterval) {
+                case 0:
+                    return [value, t, p];
+                case 1:
+                    return [q, value, p];
+                case 2:
+                    return [p, value, t];
+                case 3:
+                    return [p, q, value];
+                case 4:
+                    return [t, p, value];
+                case 5:
+                    return [value, p, q];
+                default:
+                    return [0, 0, 0];
+            }
+        }
+
+        /**
          * @param {number} red      Channel value in range [0, 256)
          */
         Color.prototype.setRed = function(red) {
@@ -146,6 +219,15 @@ define([],
         };
 
         /**
+         * @param {Color} otherColor
+         *
+         * @returns {boolean}
+         */
+        Color.prototype.equals = function(otherColor) {
+            return this.generateHexString() === otherColor.generateHexString();
+        };
+
+        /**
          *
          * @param {Color} mixinColor
          * @param {number} baseToResultRatio    [0, 1]
@@ -172,79 +254,6 @@ define([],
         function blendChannels(baseChannel, mixinChannel, baseToResultRatio) {
             return Math.round(baseChannel * baseToResultRatio + mixinChannel * (1 - baseToResultRatio));
         }
-
-        /**
-         * @returns {Color}
-         */
-        Color.prototype.copy = function() {
-            return new Color(this.getRed(), this.getGreen(), this.getBlue());
-        };
-
-        /**
-         * Builds a color object given HSV values in range [0..1].
-         *
-         * @param {number} hue
-         * @param {number} saturation
-         * @param {number} value
-         *
-         * @returns {Color}
-         */
-        Color.createFromHSV = function(hue, saturation, value) {
-            var hueInterval = Math.floor(hue * 6);
-            var f = hue * 6 - hueInterval;
-            var p = value * (1 - saturation);
-            var q = value * (1 - f * saturation);
-            var t = value * (1 - (1 - f) * saturation);
-            var coefficients = determineRgbCoefficients(hueInterval, value, p, q, t);
-            var red = Math.round(256 * coefficients[0]);
-            var green = Math.round(256 * coefficients[1]);
-            var blue = Math.round(256 * coefficients[2]);
-
-            return new Color(red, green, blue);
-        };
-
-        /**
-         * @param {number} hueInterval
-         * @param {number} value
-         * @param {number} p
-         * @param {number} q
-         * @param {number} t
-         *
-         * @returns {number[]}
-         */
-        function determineRgbCoefficients(hueInterval, value, p, q, t) {
-            switch (hueInterval) {
-                case 0:
-                    return [value, t, p];
-                case 1:
-                    return [q, value, p];
-                case 2:
-                    return [p, value, t];
-                case 3:
-                    return [p, q, value];
-                case 4:
-                    return [t, p, value];
-                case 5:
-                    return [value, p, q];
-                default:
-                    return [0, 0, 0];
-            }
-        }
-
-        /**
-         * @param {string} hexString
-         *
-         * @returns {Color}
-         */
-        Color.createFromHexString = function(hexString) {
-            var colornumberHex = hexString.substring(1);
-            var colorChannelsHex = colornumberHex.match(/.{2}/g);
-            var red = parseInt(colorChannelsHex[0], 16);
-            var green = parseInt(colorChannelsHex[1], 16);
-            var blue = parseInt(colorChannelsHex[2], 16);
-
-            return (new Color(red, green, blue));
-        };
 
         return Color;
     });
---
+++ test/ColorSpec.js
@@ -0,0 +1,42 @@
+define(['engine/graphics/Color'], function(Color) {
+    'use strict';
+
+    /**
+     * @var {Color}
+     */
+    var color;
+
+    describe('Color', function() {
+        beforeEach(function() {
+            color = new Color(0, 127, 255);
+        });
+
+        it('generates HEX string', function() {
+            expect(color.generateHexString()).toEqual('#007FFF');
+        });
+
+        it('compares to other Color', function() {
+            expect(color.equals(new Color(0, 127, 255))).toEqual(true);
+            expect(color.equals(new Color(0, 128, 255))).toEqual(false);
+        });
+
+        it('instantiates from HEX string', function() {
+            expect(color.equals(Color.createFromHexString('#007FFF'))).toEqual(true);
+        });
+
+        it('gets HEX values of channels', function() {
+            expect(color.getRedHex()).toEqual('00');
+            expect(color.getGreenHex()).toEqual('7F');
+            expect(color.getBlueHex()).toEqual('FF');
+        });
+
+        it('generates RGBA string', function() {
+            color.setAlpha(0.4);
+            expect(color.generateRgbaString()).toEqual('rgba(0, 127, 255, 0.4)');
+        });
+
+        it('blends', function() {
+            expect(color.blend(new Color(192, 1, 1), 0.5).generateHexString()).toEqual('#604080');
+        });
+    });
+});
